d87065b48fde7eb4e365825a30880039f8730813
Guilherme <guilherme.sousa009@hotmail.com>
2023-05-23 15:27:31 -0300

    - Finalizando Todo-List - Ajustando também o LyfeCycle Hook para usar para não perder os dados que usei, somente se eu limpar a lista, como aqui não foi usado o banco de dados, foi usado o armazenamento local.

--- scripts.js
+++ scripts.js
@@ -16,11 +16,20 @@ const handlingForms = {
                     done: false,
                     text: ''
                 }
+                localStorage.setItem("list", JSON.stringify(this.list))
+            }else{
+                alert('To-do text is required');
             }
         },
         clear: function () {
             this.list = [];
+        },
+        storeTodos: function () {
+            localStorage.setItem("list", JSON.stringify(this.list));
         }
+    },
+    created() {
+        this.list = localStorage.getItem("list") ? JSON.parse(localStorage.getItem("list")) : this.list;
     }
 };
 
